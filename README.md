6870f7c0a7b907f9d867cf6bfe91299766916a00
Jan Seidl <seidl@avast.com>
2017-06-28 17:15:43 +0200

    minilla scaffolder

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+RabbitMQ::Consumer::Batcher - It's new $module
+
+# SYNOPSIS
+
+    use RabbitMQ::Consumer::Batcher;
+
+# DESCRIPTION
+
+RabbitMQ::Consumer::Batcher is ...
+
+# LICENSE
+
+Copyright (C) Jan Seidl.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Jan Seidl <seidl@avast.com>
---
+++ lib/RabbitMQ/Consumer/Batcher.pm
@@ -0,0 +1,39 @@
+package RabbitMQ::Consumer::Batcher;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+RabbitMQ::Consumer::Batcher - It's new $module
+
+=head1 SYNOPSIS
+
+    use RabbitMQ::Consumer::Batcher;
+
+=head1 DESCRIPTION
+
+RabbitMQ::Consumer::Batcher is ...
+
+=head1 LICENSE
+
+Copyright (C) Jan Seidl.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jan Seidl E<lt>seidl@avast.comE<gt>
+
+=cut
+
